f6a8b3e07fcf1a5aa9eec0e187079bfe7bc4f07f
alianwar08 <alianwar08@gmail.com>
2023-02-23 21:26:46 +0400

    Add project files.

---
+++ eCom.Infrastructure/InventoryDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using eCom.Infrastructure.Persistance.Repositories;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+
+namespace eCom.Infrastructure.Persistance
+{
+    public class InventoryDbContext : DbContext
+    {
+
+        public InventoryDbContext() : base()
+        {
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var ConnectionString = @"data source = aa-dbsrv-ecom.database.windows.net; initial catalog = aa-adb-ecom; persist security info = True; user id = ecom; password = xuD2%dxuD2%d; multipleactiveresultsets = True;TrustServerCertificate=True; application name =$Application_Name$";
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrdersProducts>()
+                .HasKey(a => new { a.order_id, a.product_id });
+        }
+
+
+    }
+
+}
